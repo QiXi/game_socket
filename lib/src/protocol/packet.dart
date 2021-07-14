4d8a7b7709eff155655e869e26c154dd3f3f9486
Stanislav <qixi@mail.ru>
2021-07-15 00:29:17 +0300

    remov late

--- lib/src/protocol/packet.dart
+++ lib/src/protocol/packet.dart
@@ -2,14 +2,14 @@ import 'message.dart';
 import 'schema.dart';
 
 class Packet extends Message {
-  late int schemaCode;
-  late int schemaVersion;
+  int? schemaCode;
+  int? schemaVersion;
   int? bitMask;
 
   Packet(Schema schema) : super(schema);
 
   @override
   String toString() {
-    return 'Packet{[$schemaCode.$schemaVersion $namespace], bit:$bitMask, bool:$boolMask, int:$intList, string:$stringList}';
+    return 'Packet{[$schemaCode.$schemaVersion $namespace], bit:$bitMask, bool:$boolMask, int:$intList, string:$stringList, $payloadBytes}';
   }
 }
